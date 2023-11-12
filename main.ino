fd375a8f84360f67fd00c4349db54eb9627e2a52
MaiteSaviciusM <126886636+MaiteSaviciusM@users.noreply.github.com>
2023-11-12 20:10:07 -0300

    Update main.ino

--- main.ino
+++ main.ino
@@ -1,63 +1,68 @@
-#include <WiFi.h>
-#include <HTTPClient.h>
-
-// WiFi Configuration
-char wifiSsid[] = "WIFI123";     // Nome da rede WiFi
-char wifiPass[] = "123SENHA";    // Senha da rede WiFi
-
-// TagoIO Configuration
-char serverAddress[] = "https://admin.tago.io/devices/650772f65923c9000f66943d";  // Endereço da API da TagoIO
-char contentHeader[] = "application/json";
-char tokenHeader[] = "19b31fa1-ae02-4b12-abd7-ecfabfd37124";  // Token de autenticação da TagoIO
-
-// Define o pino do sensor de poluição do ar
-const int gasSensorPin = A0;
-
-// Inicializa o cliente HTTP
-HTTPClient client;
-
+#include "ArduinoJson.h"
+#include "EspMQTTClient.h"
+// MQTT Configuracoes
+EspMQTTClient client{
+  "Wokwi-GUEST", //SSID do WiFi
+  "",     // Senha do wifi
+  "mqtt.tago.io",  // Endereço do servidor
+  "Default",       // Usuario
+  "5bfdba81-5080-456e-ad16-bcc86a7c75aa",         // Token do device
+  "AAAAAAA",           // Nome do device
+  1883             // Porta de comunicação
+};
 void setup() {
   Serial.begin(9600);
-  init_wifi(); // Inicializa a conexão WiFi
-}
-
-void loop() {
-  int pollutionValue = analogRead(gasSensorPin); // Leitura do sensor de poluição do ar
-  send_pollution_data(pollutionValue); // Envio dos dados de poluição do ar para a TagoIO
-  delay(5000); // Aguarda 5 segundos antes de enviar o próximo conjunto de dados
-}
-
-void init_wifi() {
-  Serial.println("Conectando Wifi...");
-  Serial.print("SSID: ");
-  Serial.println(wifiSsid);
-  Serial.print("PASS: ");
-  Serial.println(wifiPass);
-  WiFi.begin(wifiSsid, wifiPass);
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
+  Serial.println("Conectando WiFi");
+  while (!client.isWifiConnected()) {
+    Serial.print('.'); client.loop(); delay(1000);
   }
-  Serial.println("WiFi Conectado!");
-  Serial.print("IP: ");
-  Serial.println(WiFi.localIP());
+  Serial.println("WiFi Conectado");
+  Serial.println("Conectando com Servidor MQTT");
+  while (!client.isMqttConnected()) {
+    Serial.print('.'); client.loop(); delay(1000);
+  }
+  Serial.println("MQTT Conectado");
 }
-
-void send_pollution_data(int pollutionValue) {
-  char postData[300];
-  int statusCode = 0;
-  
-  // Construa o JSON com os dados de poluição do ar
-  snprintf(postData, sizeof(postData), "{\n\t\"variable\": \"Pollution\",\n\t\"value\": %d,\n\t\"unit\": \"UnidadeDeMedida\"\n\t}\n", pollutionValue);
-
-  client.begin(serverAddress);
-  client.addHeader("Content-Type", contentHeader);
-  client.addHeader("Device-Token", tokenHeader);
-  statusCode = client.POST(postData);
-  
-  // Leia o código de status e o corpo da resposta
-  Serial.print("Status code: ");
-  Serial.println(statusCode);
-  Serial.println("End of POST to TagoIO");
-  Serial.println();
+// Callback da EspMQTTClient
+void onConnectionEstablished() {}
+ 
+char bufferJson[100];
+int temperatura = 0;
+int poluicao = 0;  // Adicione a variável de poluição
+int umidade = 0;   // Adicione a variável de umidade
+ 
+void loop() {
+  temperatura = random(10, 30);
+  poluicao = random(0, 100);  // Gere um valor aleatório para poluição (ajuste os limites conforme necessário)
+  umidade = random(20, 80);   // Gere um valor aleatório para umidade (ajuste os limites conforme necessário)
+ 
+  StaticJsonDocument<300> documentoJson;
+  documentoJson["variable"] = "temperatura";
+  documentoJson["value"] = temperatura;
+  documentoJson["unit"] = "celsius";
+  serializeJson(documentoJson, bufferJson);
+  Serial.println(bufferJson);
+  client.publish("topicoTDSPI", bufferJson);
+  delay(5000);
+  client.loop();
+ 
+  // Agora, crie um JSON para poluição e publique os dados
+  documentoJson["variable"] = "poluicao";
+  documentoJson["value"] = poluicao;
+  documentoJson["unit"] = "ppm";  // Unidade apropriada para poluição
+  serializeJson(documentoJson, bufferJson);
+  Serial.println(bufferJson);
+  client.publish("topicoTDSPI", bufferJson);
+  delay(5000);
+  client.loop();
+ 
+  // Repita o processo para umidade
+  documentoJson["variable"] = "umidade";
+  documentoJson["value"] = umidade;
+  documentoJson["unit"] = "%";  // Unidade apropriada para umidade
+  serializeJson(documentoJson, bufferJson);
+  Serial.println(bufferJson);
+  client.publish("topicoTDSPI", bufferJson);
+  delay(5000);
+  client.loop();
 }
